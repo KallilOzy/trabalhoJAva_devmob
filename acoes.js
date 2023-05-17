54f8dc7182eb7a42aa37232db16f651d79f9abb8
KallilOzy <129187643+KallilOzy@users.noreply.github.com>
2023-05-17 09:35:55 -0300

    Add files via upload

---
+++ acoes.js
@@ -0,0 +1,19 @@
+function acao1(){
+    let resp = window.document.getElementById("saída")
+    resp.innerHTML += '<p> VocÊ clicou no primeiro botão </p>'
+}
+
+function acao2(){
+    let resp = window.document.getElementById("saída")
+    resp.innerHTML += '<p> VocÊ clicou no primeiro botão </p>'
+}
+
+function acao3(){
+    let resp = window.document.getElementById("saída")
+    resp.innerHTML += '<p> VocÊ clicou no primeiro botão </p>'
+}
+
+function acao4(){
+    let resp = window.document.getElementById("saída")
+    resp.innerHTML += '<p> VocÊ clicou no primeiro botão </p>'
+}
